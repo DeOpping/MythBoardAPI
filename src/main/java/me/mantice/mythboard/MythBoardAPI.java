0729592532d7a0330a76bf5739f60f96a82ce129
Mantice <deopping@outlook.com>
2025-09-02 09:55:48 -0400

    feat: javadoc for getApi

--- src/main/java/me/mantice/mythboard/MythBoardAPI.java
+++ src/main/java/me/mantice/mythboard/MythBoardAPI.java
@@ -7,6 +7,11 @@ import org.jetbrains.annotations.Nullable;
 
 public interface MythBoardAPI {
 
+    /**
+     * Get MythBoard's implementation of the {@link MythBoardAPI}
+     * @return {@link MythBoardAPI}
+     * @throws IllegalStateException if the API is not registered. Typically, this only happens if the MythBoard plugin is not enabled yet
+     */
     static @NotNull MythBoardAPI getApi() {
         final MythBoardAPI api = Bukkit.getServicesManager().load(MythBoardAPI.class);
         if (api == null) {
